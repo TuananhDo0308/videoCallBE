e061467e52cf3203d3a82b37838f490d534c89b1
TuananhDo <TuananhDo0308@gmail.com>
2025-04-20 09:27:46 +0700

    update

--- src/main/java/com/example/demo/Config/CorsConfig.java
+++ src/main/java/com/example/demo/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
-                        .allowedOrigins("https://f71a-171-252-189-105.ngrok-free.app") // Chỉ định origin cụ thể
+                        .allowedOrigins("*") // Chỉ định origin cụ thể
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép các phương thức HTTP
                         .allowedHeaders("*") // Cho phép tất cả header
                         .allowCredentials(true); // Cho phép gửi credentials
